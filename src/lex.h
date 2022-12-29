66f1c49034f099be8d1a6d0495e25510da57444c
jackietao <jackietao@tencent.com>
2022-12-29 10:50:20 +0800

    feat:添加token标志

--- src/lex.h
+++ src/lex.h
@@ -5,8 +5,35 @@
 
 //token枚举
 enum token_type {
-    IDENT = 1,
-    BRACE = 2,
+    IDENT = 1, // identifier
+
+    // 关键字
+    AND,
+    BREAK,
+    DO,
+    ELSE,
+    ELSEIF,
+    END,
+    FALSE,
+    FOR,
+    FUNCTION,
+    GOTO,
+    IF,
+    IN,
+    LOCAL,
+    NIL,
+    NOT,
+    OR,
+    REPEAT,
+    RETURN,
+    THEN,
+    TRUE,
+    UNTIL,
+    WHILE,
+
+    // 操作符
+    PLUS,
+    MINUS,
 };
 
 //token信息
